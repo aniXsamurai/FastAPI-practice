f5b0854dd6e05fd2c7ce63a809ac7dbfc9119ea8
aniruddha <aniruddhagarai312@gmail.com>
2026-06-08 21:53:52 +0530

    learned about basic Field and annotated

--- Pydantic/B1_typeValidation.py
+++ Pydantic/B1_typeValidation.py
@@ -1,14 +1,22 @@
-from pydantic import BaseModel
-from typing import List,Dict,Optional
+#----Notes----
+# 1.EmailStr needs an external library : pip install "pydantic[email]"
+# 2.we can add meta data using the combination of Annotated and Field
+# 3. Field (Strict = True): To block auto type conversion like: age -> input:'23',raise error 
+
+
+from pydantic import BaseModel,EmailStr,AnyUrl,Field
+from typing import List,Dict,Optional,Annotated
 
 # Define the structure of a student record
 class Student(BaseModel):
-    name: str
-    age: int
-    skills: List[str] #Typing List used to specify the type of the values too
-    contact: Dict[str,str]
-    married : bool = False # Default value is false
-    hobbies: Optional[List[str]] = None #Optional is used , if user doesnt pass any value it will consider None
+    name: str = Field(max_length=50)
+    age: int = Field(gt=15,lt=30)
+    email : EmailStr   # Insead of regex or other complex code 
+    linkedin: AnyUrl   # It will validate the data provied it a link
+    skills: List[str]  #Typing List used to specify the type of the values too
+    contact: Dict[str,str] = Field(max_length=2)
+    married : Annotated[bool,Field(default=False,description="Student is married or not.")]  # Default value is false
+    hobbies: Optional[List[str]] = None  #Optional is used , if user doesnt pass any value it will consider None
 
 
 # Function to display student information
@@ -19,6 +27,8 @@ def display_student_info(student: Student):
     print("contact :", student.contact)
     print("married:", student.married)
     print("hobbies :", student.hobbies) 
+    print("Email :", student.email) 
+    print("linkedin :", student.linkedin) 
 
 
 # Student data received as a dictionary
@@ -26,13 +36,19 @@ student_data = {
     "name": "Ani",
     "age": "24", # pydantic will convert this str-->int , if mistakenly you passes str
     "skills" : ['python','fastAPI'],
-    "contact":{'email':"ani@gmail.com",'ph':'18837438'}
+    "contact":{'email':"ani@gmail.com",'ph':'18837438'},
     #married value is not sent
     #Hobbies also not sent
+    #Throws error if format is wrong 
+    "linkedin":'https://linkedin.com/in/aniruddha-garai',
+    "email":'ani@gmail.com' 
 }
 
 # Create a Student object from the dictionary
 student_record = Student(**student_data)  # ** unpacks the dictionary
 
 # Display the student details
-display_student_info(student_record)
+#display_student_info(student_record)
+
+#we can use model_dump() to print record instead printing all separately
+print(student_record.model_dump())
